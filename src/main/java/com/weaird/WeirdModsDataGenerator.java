9ba9effb42b560e63e058a76108e73bf2cf43978
ADDAP0 <3840447667@qq.com>
2026-02-04 15:48:36 +0800

    Initial commit

---
+++ src/main/java/com/weaird/WeirdModsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.weaird;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WeirdModsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
